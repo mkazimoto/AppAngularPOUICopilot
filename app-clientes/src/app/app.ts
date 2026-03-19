2c15ee0560e8e371858a5140bd11dc955645f9c9
MARCOS KAZIMOTO <marcos.kazimoto@totvs.com.br>
2026-03-19 08:32:39 -0300

    refactor: remove Reservas module and associated files for hotel reservations

--- app-clientes/src/app/app.ts
+++ app-clientes/src/app/app.ts
@@ -20,7 +20,6 @@ export class App {
     { label: 'Aluguel de Carros', link: '/carros', icon: 'an an-car' },
     { label: 'Cursos Online', link: '/cursos', icon: 'an an-graduation-cap' },
     { label: 'Imóveis', link: '/imoveis', icon: 'an an-building' },
-    { label: 'Reservas de Hotel', link: '/reservas', icon: 'an an-bed' },
   ];
 
 
